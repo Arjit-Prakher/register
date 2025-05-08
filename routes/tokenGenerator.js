5da4dafa23c83747c6e697b41ae3869b19e8876c
Arjit Prakher <arjitprakher4@gmail.com>
2025-05-08 22:00:25 +0530

    initial commit

---
+++ routes/tokenGenerator.js
@@ -0,0 +1,43 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+let apiToken = null;
+let tokenExpiry = null;
+
+const API_KEY = process.env.API_KEY;
+
+async function generateNewToken() {
+    try {
+        const formData = new URLSearchParams({
+            grant_type: "urn:ibm:params:oauth:grant-type:apikey",
+            apikey: API_KEY
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: formData
+        };
+
+        const response = await fetch('https://iam.cloud.ibm.com/identity/token#apikey', requestOptions);
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getToken() {
+    if (Date.now() >= tokenExpiry * 1000) {
+        console.log("Token was expired, requesting new...");
+        const newToken = await generateNewToken();
+        apiToken = newToken?.access_token;
+        tokenExpiry = newToken?.expiration;
+    } else {
+        console.log("Token is fresh");
+    }
+    return apiToken;
+}
+module.exports = getToken;
